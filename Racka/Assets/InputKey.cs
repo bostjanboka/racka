d467f1a3d7b2d57dc4816bd477f7383df0c699e4
bostjanboka <bohtebostjan@gmail.com>
2015-06-07 19:51:21 +0200

    wasd kontrole in koncept račk

---
+++ Racka/Assets/InputKey.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputKey : MonoBehaviour {
+
+	// Use this for initialization
+	static public bool w;
+	static public bool a;
+	static public bool s;
+	static public bool d;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		w = Input.GetKey("w");
+
+		a = Input.GetKey("a");
+
+		s = Input.GetKey("s");
+
+		d = Input.GetKey("d");
+
+		
+	}
+}
---
+++ Racka/Assets/RackaSkripta.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RackaSkripta : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+	Vector3 smer;
+	void Start () {
+		smer = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (InputKey.w) {
+			smer.z = 1;
+		}
+		if (InputKey.a) {
+			smer.x = -1;
+		}
+		if (InputKey.s) {
+			smer.z = -1;
+		}
+		if (InputKey.d) {
+			smer.x = 1;
+		}
+
+		smer.Normalize ();
+		smer *= speed;
+
+		gameObject.GetComponent<Rigidbody> ().velocity = smer;
+		smer = Vector3.zero;
+	}
+}
