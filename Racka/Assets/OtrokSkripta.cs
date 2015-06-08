d95a5b90d28ce9b46db622f1e72ca3f44dcbeb07
bostjanboka <bohtebostjan@gmail.com>
2015-06-08 22:26:53 +0200

    skripte

---
+++ Racka/Assets/OtrokSkripta.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OtrokSkripta : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject zasleduj;
+	Vector3 smer;
+	public float speed;
+	void Start () {
+		smer = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (zasleduj) {
+
+			smer = zasleduj.transform.position - transform.position;
+			if(smer.magnitude > 2.5f){
+				smer.Normalize();
+				smer = smer*speed;;
+				gameObject.GetComponent<Rigidbody>().velocity = smer;
+			}
+		}
+	}
+}
