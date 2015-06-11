d2473512c49d9d3b1596bdbe9413069fc35252ff
bostjanboka <bohtebostjan@gmail.com>
2015-06-11 18:21:35 +0200

    voda kot gameobject

--- Racka/Assets/RackaSkripta.cs
+++ Racka/Assets/RackaSkripta.cs
@@ -7,47 +7,54 @@ public class RackaSkripta : MonoBehaviour {
 	public float speed;
 	float smer;
 	Vector3 rotacija;
-	Vector3 premik;
+	float premik;
 	//public GameObject kamera;
+	GameObject valovi;
 	void Start () {
 
 		rotacija = Vector3.zero;
-
+		valovi = transform.FindChild ("valovi").gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-
-		//rotacija = Vector3.zero;
-		premik = Vector3.zero;
+		premik = 0;
 		float rot = 0;
 
 		if (InputKey.w) {
 
-			premik.z += -1*speed;
+			premik=1;
 		}
 		if (InputKey.a) {
 			rot -= 40*Time.deltaTime;
 		}
 		if (InputKey.s) {
-			premik.z += 1*speed;
+			premik = -1;
 		}
 		if (InputKey.d) {
 			rot += 40*Time.deltaTime;
 		}
 
-
-
-
-
-		premik.Normalize ();
-
 		gameObject.transform.Rotate (0,rot,0);
-		gameObject.transform.Translate(premik * speed*Time.deltaTime);
-		//gameObject.transform.Rotate(rotacija);
-		//kamera.transform.Translate (premik * smer*Time.deltaTime);
-
+		transform.position += transform.forward * speed * premik * Time.deltaTime;
 
 	}
+
+	void OnTriggerStay(Collider other) {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag.Equals ("voda")) {
+			valovi.SetActive (true);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag.Equals ("voda")) {
+			valovi.SetActive (false);
+		}
+	}
+
 }
