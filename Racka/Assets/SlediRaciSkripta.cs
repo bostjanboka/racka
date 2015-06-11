401f8d3f28e158e79c8748a98025ec49bfb38bc4
bostjanboka <bohtebostjan@gmail.com>
2015-06-11 19:54:31 +0200

    kamera

---
+++ Racka/Assets/SlediRaciSkripta.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlediRaciSkripta : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject raca;
+	public float oddaljenost=6;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = raca.transform.position;
+		transform.position += Vector3.up * oddaljenost;
+		transform.position += Vector3.forward * -20;
+	}
+}
