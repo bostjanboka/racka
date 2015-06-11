f16b9350898761bb2a4ce29e61c7d82cf1b80f91
bostjanboka <bohtebostjan@gmail.com>
2015-06-11 14:15:47 +0200

    premikanje colna in spawn pointa

---
+++ Racka/Assets/SkriptaPotujNaprej.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkriptaPotujNaprej : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed=4;
+	public float zbrisiPoCasu = 20;
+
+	float casNastanka;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position+= (transform.forward * speed * Time.deltaTime);
+
+		zbrisiPoCasu -= Time.deltaTime;
+		if (zbrisiPoCasu <= 0) {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Racka/Assets/SpawnGameObjectSkripta.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnGameObjectSkripta : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject objekt;
+	public float casRespawna=5;
+
+	float cas;
+	void Start () {
+		cas = casRespawna;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cas -= Time.deltaTime;
+		if (cas <= 0) {
+			Instantiate(objekt, transform.position, transform.rotation);
+
+			cas=casRespawna;
+		}
+	}
+}
