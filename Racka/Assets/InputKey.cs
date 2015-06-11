352f07c7010df42181770a7501af12d1363da621
bostjanboka <bohtebostjan@gmail.com>
2015-06-11 21:22:28 +0200

    premikanje po mobitelu

--- Racka/Assets/InputKey.cs
+++ Racka/Assets/InputKey.cs
@@ -8,6 +8,9 @@ public class InputKey : MonoBehaviour {
 	static public bool a;
 	static public bool s;
 	static public bool d;
+
+	Vector2 staraPozicija;
+	Vector2 zdajPozicija;
 	void Start () {
 	
 	}
@@ -22,6 +25,23 @@ public class InputKey : MonoBehaviour {
 
 		d = Input.GetKey("d");
 
-		
+		foreach (Touch touch in Input.touches) {
+			if(touch.phase.Equals(TouchPhase.Began)){
+				staraPozicija = touch.position;
+			}else if(touch.phase.Equals(TouchPhase.Moved) || touch.phase.Equals(TouchPhase.Stationary)){
+				zdajPozicija = touch.position;
+				if(zdajPozicija.y - staraPozicija.y > 50){
+					w=true;
+				}else if(zdajPozicija.y - staraPozicija.y < -50){
+					s = true;
+				}
+
+				if(zdajPozicija.x - staraPozicija.x > 50){
+					d=true;
+				}else if(zdajPozicija.x - staraPozicija.x < -50){
+					a = true;
+				}
+			}
+		}
 	}
 }
