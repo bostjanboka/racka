59617b06e2791c06f2fe8deeff19a9aa04a465ac
bostjanboka <bohtebostjan@gmail.com>
2015-06-17 14:44:53 +0200

    update

--- Racka/Assets/RandomCreatorSkripta.cs
+++ Racka/Assets/RandomCreatorSkripta.cs
@@ -1,20 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RandomCreatorSkripta : MonoBehaviour {
 
 	// Use this for initialization
+	GameObject raca;
+
 	public GameObject cesta;
 	public GameObject voda;
 
 	GameObject[] tabela;
 	Vector3 vec;
+	List<GameObject> list;
+	int brisi=0;
 	void Start () {
+
+		raca = GameObject.Find ("raca");
+		list = new List<GameObject>();
 		vec = Vector3.zero;
 		tabela = new GameObject[2];
 		tabela [0] = cesta;
 		tabela [1] = voda;
-
+		/*
 		for (int i=0; i < 100; i++) {
 			GameObject spawn = tabela[Random.Range(0,2)];
 			Mesh mesh = spawn.GetComponent<MeshFilter>().sharedMesh;
@@ -27,11 +35,38 @@ public class RandomCreatorSkripta : MonoBehaviour {
 			Instantiate(spawn,pozicija,Quaternion.Euler(0,0,0));
 
 
+		}*/
+
+		for (int i=0; i < 3; i++) {
+			GameObject spawn = tabela[Random.Range(0,2)];
+			Mesh mesh = spawn.GetComponent<MeshFilter>().sharedMesh;
+			Bounds bounds = mesh.bounds;
+			
+			vec.z+= bounds.size.z*spawn.transform.localScale.z;
+			
+			Vector3 pozicija = spawn.transform.position;
+			pozicija.z = vec.z - bounds.size.z/2f*spawn.transform.localScale.z;
+
+			list.Add(Instantiate(spawn,pozicija,Quaternion.Euler(0,0,0)) as GameObject);
 		}
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (list[list.Count-2].transform.position.z < raca.transform.position.z) {
+			GameObject spawn = tabela[Random.Range(0,2)];
+			Mesh mesh = spawn.GetComponent<MeshFilter>().sharedMesh;
+			Bounds bounds = mesh.bounds;
+			
+			vec.z+= bounds.size.z*spawn.transform.localScale.z;
+			
+			Vector3 pozicija = spawn.transform.position;
+			pozicija.z = vec.z - bounds.size.z/2f*spawn.transform.localScale.z;
+			
+			list.Add(Instantiate(spawn,pozicija,Quaternion.Euler(0,0,0)) as GameObject);
+			Destroy(list[brisi++]);
+
+		}
 	}
 }
