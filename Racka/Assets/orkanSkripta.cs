96215a29111eadb29d2379d569349968dfe062f8
bostjanboka <bohtebostjan@gmail.com>
2015-06-23 16:16:06 +0200

    orkan

---
+++ Racka/Assets/orkanSkripta.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class orkanSkripta : MonoBehaviour {
+
+	// Use this for initialization
+	public float histrostVrtenja;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0,histrostVrtenja*Time.deltaTime,0);
+
+	}
+}
