d369fbf76cf549557b50752377a736c15d1cd390
bostjanboka <bohtebostjan@gmail.com>
2015-06-23 21:52:59 +0200

    orkan se premika random

--- Racka/Assets/orkanSkripta.cs
+++ Racka/Assets/orkanSkripta.cs
@@ -5,12 +5,20 @@ public class orkanSkripta : MonoBehaviour {
 
 	// Use this for initialization
 	public float histrostVrtenja;
+	public float speed = 3;
+	Vector3 premakni;
 	void Start () {
-	
+		premakni = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Vector3.Distance (premakni, transform.position) < 0.1) {
+			premakni.x += Random.Range (-5, 6);
+			premakni.z += Random.Range (-5, 6);
+		} else {
+			transform.position = Vector3.MoveTowards(transform.position,premakni,speed*Time.deltaTime);
+		}
 		transform.Rotate (0,histrostVrtenja*Time.deltaTime,0);
 
 	}
