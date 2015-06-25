f01596129feb5a4517d06af6d9aa684c08efc113
bostjanboka <bohtebostjan@gmail.com>
2015-06-25 21:45:00 +0200

    ORKAN

--- Racka/Assets/orkanSkripta.cs
+++ Racka/Assets/orkanSkripta.cs
@@ -6,8 +6,7 @@ public class orkanSkripta : MonoBehaviour {
 	// Use this for initialization
 	public float histrostVrtenja;
 	public float speed = 3;
-	public GameObject rackaVOrkanu;
-	public GameObject glavnaRacaVOrkanu;
+	public GameObject vOrkanu;
 	Vector3 premakni;
 	void Start () {
 		premakni = transform.position;
@@ -23,21 +22,38 @@ public class orkanSkripta : MonoBehaviour {
 		}
 		transform.Rotate (0,histrostVrtenja*Time.deltaTime,0);
 
+
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other.tag.Equals ("raca")) {
-			GameObject game = Instantiate(glavnaRacaVOrkanu,Vector3.zero,Quaternion.Euler(0,0,0)) as GameObject;
-			game.transform.parent = transform;
+		if (other.tag.Equals ("raca") || other.tag.Equals ("otrok") || other.tag.Equals ("coln")|| other.tag.Equals ("avto") || other.tag.Equals ("drevo")) {
+			/*GameObject game = Instantiate(glavnaRacaVOrkanu,Vector3.zero,Quaternion.Euler(0,0,0)) as GameObject;*/
+			GameObject game = sestavi(other.gameObject,gameObject);
+			//game.transform.parent = transform;
 			game.transform.position = other.transform.position - game.transform.position;
 			Destroy(other.gameObject);
 
 		}
-		else if (other.tag.Equals ("otrok")) {
-			GameObject game = Instantiate(rackaVOrkanu,Vector3.zero,Quaternion.Euler(0,0,0)) as GameObject;
-			game.transform.parent = transform;
-			game.transform.position = other.transform.position - game.transform.position;
-			Destroy(other.gameObject);
+	}
+
+	GameObject sestavi(GameObject obj, GameObject stars){
+		GameObject game = Instantiate(vOrkanu,Vector3.zero,Quaternion.Euler(0,0,0)) as GameObject;
+		if (obj.GetComponent<MeshFilter> ()) {
+			game.GetComponent<MeshFilter> ().mesh = obj.GetComponent<MeshFilter> ().mesh;
 		}
+		if (obj.GetComponent<MeshRenderer> ()) {
+			game.GetComponent<MeshRenderer> ().material = obj.GetComponent<MeshRenderer> ().material;
+		}
+		if (stars != null) {
+			game.transform.parent = stars.transform;
+		}
+		for (int i=0; i < obj.transform.childCount; i++) {
+			GameObject otrok = sestavi(obj.transform.GetChild(i).gameObject,game);
+			otrok.GetComponent<vOrkanuSkripta>().enable=false;
+			otrok.transform.localPosition = obj.transform.GetChild(i).transform.localPosition;
+			otrok.transform.localRotation = obj.transform.GetChild(i).transform.localRotation;
+			otrok.transform.localScale = obj.transform.GetChild(i).transform.localScale;
+		}
+		return game;
 	}
 }
--- Racka/Assets/vOrkanuSkripta.cs
+++ Racka/Assets/vOrkanuSkripta.cs
@@ -7,18 +7,38 @@ public class vOrkanuSkripta : MonoBehaviour {
 	public float kot;
 	public float speed;
 	public float trajanje = 7;
+	public bool enable=true;
+
+	Transform orkan=null;
 	void Start () {
-		transform.Rotate (kot, 0, 0);
+		if (enable) {
+			transform.Rotate (kot, 0, 0);
+		}
+		orkan = transform.parent;
+		while (orkan.parent != null) {
+			orkan = orkan.parent;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position += transform.up * speed * Time.deltaTime;
-
 		trajanje -= Time.deltaTime;
-		if (trajanje < 0) {
-			Destroy(gameObject);
+		if (trajanje < 6 && !enable) {
+			sirota();
 		}
+		if (transform.position.y > 15) {
+			Destroy (gameObject);
+		}
+		if (enable) {
+			transform.position += transform.up * speed * Time.deltaTime;
+		}
+	}
+
+	void sirota(){
+		Debug.Log ("sirota");
+		transform.parent = orkan;
+		enable = true;
+		speed = Random.Range (2.0f, 3.1f);
 	}
 
 
