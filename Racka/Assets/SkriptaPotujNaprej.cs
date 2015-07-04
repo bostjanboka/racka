e76374be40f1a211c6d6fd8e10b5392824004912
bostjanboka <bohtebostjan@gmail.com>
2015-07-04 20:01:04 +0200

    save

--- Racka/Assets/SkriptaPotujNaprej.cs
+++ Racka/Assets/SkriptaPotujNaprej.cs
@@ -5,7 +5,7 @@ public class SkriptaPotujNaprej : MonoBehaviour {
 
 	// Use this for initialization
 	public float speed=4;
-	public float zbrisiPoCasu = 40;
+	float zbrisiPoCasu = 60;
 
 	float casNastanka;
 	void Start () {
