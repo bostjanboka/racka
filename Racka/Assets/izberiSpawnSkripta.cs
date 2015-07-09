818a0faa02730a5875688bdd62464b17bed0f71d
bostjanboka <bohtebostjan@gmail.com>
2015-07-10 01:56:46 +0200

    izbira spawna na random, cesta ne bo na random

---
+++ Racka/Assets/izberiSpawnSkripta.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class izberiSpawnSkripta : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SpawnGameObjectSkripta[] spawn1 = GetComponentsInChildren<SpawnGameObjectSkripta> ();
+		if (spawn1.Length > 0) {
+			spawn1 [Random.Range (0, 2)].enabled = false;
+		} else {
+			spawnColnSkripta[] spawn2 = GetComponentsInChildren<spawnColnSkripta> ();
+			if(spawn2.Length > 0){
+				spawn2 [Random.Range (0, 2)].enabled = false;
+			}else{
+				spawnVlakSkripta[] spawn3 = GetComponentsInChildren<spawnVlakSkripta> ();
+				spawn3 [Random.Range (0, 2)].enabled = false;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
