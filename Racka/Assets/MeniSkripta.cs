8f111d6557d569758c4a948e751c66015329dc78
bostjanboka <bohtebostjan@gmail.com>
2015-08-05 14:13:24 +0200

    UPDATE

--- Racka/Assets/MeniSkripta.cs
+++ Racka/Assets/MeniSkripta.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class MeniSkripta : MonoBehaviour {
 
 	// Use this for initialization
+	public GameObject kamera;
 	public GameObject meni;
 	public GameObject hud;
 	public GameObject loose;
@@ -12,9 +13,13 @@ public class MeniSkripta : MonoBehaviour {
 	public Text bestScore;
 	public Text Score;
 	public Text stRack;
+
+	float tocke;
+	float zacetna;
 	void Awake(){
 		hud.SetActive (false);
 		loose.SetActive (false);
+		zacetna = kamera.transform.position.z;
 	}
 	void Start () {
 	
@@ -23,6 +28,7 @@ public class MeniSkripta : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		stRack.text = RackaSkripta.stRack+" / 10";
+
 	}
 
 
