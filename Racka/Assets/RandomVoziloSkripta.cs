788e602fc1f786c3cc0ec73037fb9ac8bcf3c129
bostjanboka <bohtebostjan@gmail.com>
2015-08-11 20:52:46 +0200

    vsi avti

--- Racka/Assets/RandomVoziloSkripta.cs
+++ Racka/Assets/RandomVoziloSkripta.cs
@@ -3,27 +3,12 @@ using System.Collections;
 
 public class RandomVoziloSkripta : MonoBehaviour {
 
-	// Use this for initialization
-	public GameObject avto1;
-	public GameObject avto2;
-	public GameObject avto3;
-	public GameObject avto4;
-	public GameObject avto5;
-	public GameObject avto6;
-	public GameObject avto7;
-	public GameObject avto8;
 
-	GameObject[] tabela;
+
+	public GameObject[] tabela;
+	
 	void Awake(){
-		tabela = new GameObject[8];
-		tabela [0] = avto1;
-		tabela [1] = avto2;
-		tabela [2] = avto3;
-		tabela [3] = avto4;
-		tabela [4] = avto5;
-		tabela [5] = avto6;
-		tabela [6] = avto7;
-		tabela [7] = avto8;
+
 	}
 	void Start () {
 
