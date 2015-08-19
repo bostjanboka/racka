bcb89980db1c561ceb4733a9f254e55de0ccf270
bostjanboka <bohtebostjan@gmail.com>
2015-08-19 21:13:30 +0200

    Merge remote-tracking branch 'origin/boka' into bokart
    Conflicts:
    	Racka/Assets/svetBB.unityr
    
    kaj

---
+++ Racka/Assets/reklame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class reklame : MonoBehaviour {
+
+	// Use this for initialization
+
+	void Start () {
+		//UnityPluginForWindowsPhone.Class1.konstruktor ("ca-app-pub-6223160944701050/5027679925",true);
+		//UnityPluginForWindowsPhone.Class1.loadCelozaslonsko ();
+
+		UnityPluginForWindowsPhone.Class1 x = new UnityPluginForWindowsPhone.Class1 ();
+		//x.klas ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//UnityPluginForWindowsPhone.Class1.showCelozaslonsko ();
+	}
+}
