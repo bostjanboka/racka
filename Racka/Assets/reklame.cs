a65786633968fea7fc1f0abd1bbb97740175617b
bostjanboka <bohtebostjan@gmail.com>
2015-08-22 23:03:47 +0200

    reklame

--- Racka/Assets/reklame.cs
+++ Racka/Assets/reklame.cs
@@ -11,7 +11,7 @@ public class reklame : MonoBehaviour {
 		//UnityPluginForWindowsPhone.Class1.konstruktor ("ca-app-pub-6223160944701050/5027679925",true);
 		//UnityPluginForWindowsPhone.Class1.loadCelozaslonsko ();
 
-		UnityPluginForWindowsPhone.Class1.klas ();
+		UnityPluginForWindowsPhone.Class1.createBanner (false,"ca-app-pub-6223160944701050/4726628723",true);
 		//x.klas ();
 
 	}
@@ -19,5 +19,21 @@ public class reklame : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//UnityPluginForWindowsPhone.Class1.showCelozaslonsko ();
+	}
+
+	public void prizgi(){
+		UnityPluginForWindowsPhone.Class1.setBannerVisibility (true);
+	}
+
+	public void ugasni(){
+		UnityPluginForWindowsPhone.Class1.setBannerVisibility (false);
+	}
+
+	public void dol(){
+		UnityPluginForWindowsPhone.Class1.setBannerAlignment ("BOTTOM");
+	}
+
+	public void gor(){
+		UnityPluginForWindowsPhone.Class1.setBannerAlignment ("TOP");
 	}
 }
